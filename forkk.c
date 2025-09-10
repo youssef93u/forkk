47bdd5bf2b3f1967905f4e2c3c410e2e76be2505
youssef93u <youssef93oulaarabi@gmail.com>
2025-09-10 16:58:41 +0200

    original

--- forkk.c
+++ forkk.c
@@ -1,9 +1,7 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-    // Write C code here
-    printf("Try programiz.pro");
+    printf("original repo");
 
     return 0;
 }
